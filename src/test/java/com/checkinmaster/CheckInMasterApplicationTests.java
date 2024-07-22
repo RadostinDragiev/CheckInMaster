12ae242aa7506a973d00e713601d2b8cf78f9036
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2024-07-22 23:19:58 +0300

    Initial commit

---
+++ src/test/java/com/checkinmaster/CheckInMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkinmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
